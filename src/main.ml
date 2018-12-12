5547880518b93a20842c834929eb10573ee65250
Christian Lindig <christian.lindig@citrix.com>
2018-12-12 17:17:49 +0000

    Implement server in main.ml for testing
    Signed-off-by: Christian Lindig <christian.lindig@citrix.com>

--- src/main.ml
+++ src/main.ml
@@ -7,22 +7,45 @@ let run = ref true
 
 let buf = Bytes.make 20 '@'
 
-let process _epoll fd events =
-  Printf.eprintf "events = %s\n%!" (Epoll.Events.to_string events) ;
-  ( if Epoll.Events.(test events inp) then
-    let n = Unix.read fd buf 0 20 in
-    Unix.write Unix.stdout buf 0 n |> ignore ) ;
-  if Epoll.Events.(test events out) then
-    Unix.write_substring fd "hello\n" 0 6 |> ignore ;
-  if Epoll.Events.(test events hup) then Unix.close fd
+let create_socket path =
+  let sock = Unix.(socket PF_UNIX SOCK_STREAM 0) in
+  let addr = Unix.ADDR_UNIX path in
+  Unix.bind sock addr;
+  Unix.listen sock 10;
+  sock
 
-let polly files =
+let accept epoll sock =
+  let fd, _ = Unix.accept sock in
+  Epoll.add epoll fd Epoll.Events.(inp)
+
+let (+++) = Epoll.Events.(lor)
+let ready = Epoll.Events.(inp +++ hup)
+let other = Epoll.Events.(lnot ready)
+
+let process sock epoll fd events =
+  if fd = sock && Epoll.Events.(test events inp) then begin
+      accept epoll sock;
+  end else begin 
+    if Epoll.Events.(test events ready) then begin
+      match Unix.read fd buf 0 20 with
+      | 0 -> Unix.close fd
+      | n -> Unix.write Unix.stdout buf 0 n |> ignore
+    end;
+    if Epoll.Events.(test events other) then begin
+      Unix.close fd
+    end
+  end
+
+let polly path =
   let epoll = Epoll.create () in
-  let fds = files |> List.map (fun x -> Unix.openfile x [Unix.O_RDONLY] 0) in
-  let add fd = Epoll.add epoll fd Epoll.Events.(inp) in
-  let _ = List.iter add fds in
+  let sock  = create_socket path in
+  let clean () = try Unix.unlink path with _ -> () in
+  at_exit clean;
+  Epoll.add epoll sock Epoll.Events.(inp);
   while !run do
-    Epoll.wait epoll 10 timeout process |> ignore
+    match Epoll.wait epoll 10 timeout (process sock) with
+    | _ -> ()
+    | exception Unix.Unix_error (Unix.EINTR, _, _) -> ()
   done
 
 module Command = struct
@@ -30,23 +53,27 @@ module Command = struct
     [ `S "BUGS"
     ; `P "Check bug reports at https://github.com/lindig/polly/issues" ]
 
-  let files =
+  let path =
     C.Arg.(
-      non_empty & pos_all file []
+      value & pos 0 string "polly.sock"
       & info [] ~docv:"FILE" ~doc:"Socket to read from")
 
   let polly =
-    let doc = "Read from multiple sockets" in
-    C.Term.(const polly $ files, info "polly" ~doc ~man:help)
+    let doc = "Read from multiple connections, write to stdout" in
+    C.Term.(const polly $ path, info "polly" ~doc ~man:help)
 end
 
+
 let main () =
-  try
-    match C.Term.eval Command.polly ~catch:false with
-    | `Error _ -> exit 1
-    | _ -> exit 0
-  with exn ->
+  let signal _ = exit 1 in
+  Sys.set_signal Sys.sigterm (Sys.Signal_handle signal);
+  Sys.set_signal Sys.sigint (Sys.Signal_handle signal);
+  match C.Term.eval Command.polly ~catch:false with
+  | `Error _ -> exit 1
+  | _ -> exit 0
+  | exception exn ->
     Printf.eprintf "error: %s\n" (Printexc.to_string exn) ;
     exit 1
+;;
 
 let () = if !Sys.interactive then () else main ()
