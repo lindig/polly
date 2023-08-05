7d2e8e3dcc068e62dd973f1b8ed1bbe23121fa1c
Christophe Raffalli <christophe@raffalli.eu>
2023-08-05 13:31:08 -1000

    forgotten new file

---
+++ src/constants.c
@@ -0,0 +1,40 @@
+#include <sys/epoll.h>
+#include <sys/eventfd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void constant(const char* name, int value) {
+  printf("  let %s = 0x%x\n", name, value);
+}
+
+int main(int argc, char **argv) {
+  printf("module Epoll_constants = struct\n");
+
+  constant("inp",EPOLLIN);
+  constant("pri",EPOLLPRI);
+  constant("out",EPOLLOUT);
+  constant("rdnorm",EPOLLRDNORM);
+  constant("rdband",EPOLLRDBAND);
+  constant("wrnorm",EPOLLWRNORM);
+  constant("wrband",EPOLLWRBAND);
+  constant("msg",EPOLLMSG);
+  constant("err",EPOLLERR);
+  constant("hup",EPOLLHUP);
+  constant("rdhup",EPOLLRDHUP);
+  constant("wakeup",EPOLLWAKEUP);
+  constant("oneshot",EPOLLONESHOT);
+  constant("et",EPOLLET);
+  //constant("exclusive",EPOLLEXCLUSIVE);
+
+  printf("end\n");
+
+  printf("module EventFd_constants = struct\n");
+
+  constant("cloexec", EFD_CLOEXEC);
+  constant("nonblock", EFD_NONBLOCK);
+  constant("semaphore", EFD_SEMAPHORE);
+
+  printf("end\n");
+
+  fflush(stdout);
+}
