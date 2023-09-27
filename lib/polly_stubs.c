lindig/polly

commit 4150f121e64e04fafee525c1789945c6fceaca5f

Add eventfd (#4)
* Add eventfd

* use the CONSTANT macro

lib/polly_stubs.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <sys/resource.h>
 #include <unistd.h>
+#include <sys/eventfd.h>
 #include <caml/mlvalues.h>
 #include <caml/callback.h>
 #include <caml/memory.h>
@@ -157,5 +158,17 @@ caml_polly_wait_fold(value val_epfd, value val_max, value val_timeout,
 
 	CAMLreturn(args[3]);
 }
+
+CAMLprim value caml_eventfd(value initval, value flags)
+{
+  CAMLparam0();
+  int sock = eventfd(Int_val(initval),Int_val(flags));
+  CAMLreturn(Val_int(sock));
+}
+
+CONSTANT(EFD_CLOEXEC);
+CONSTANT(EFD_NONBLOCK);
+CONSTANT(EFD_SEMAPHORE);
+
 
 /* vim: set ts=8 noet: */

lib/polly.ml
@@ -137,3 +137,52 @@ let upd = caml_polly_mod
 let wait = caml_polly_wait
 
 let wait_fold = caml_polly_wait_fold
+
+module EventFD = struct
+  type t = Unix.file_descr
+  type flags = int
+
+  external eventfd  : int -> flags -> t = "caml_eventfd"
+
+  external efd_cloexec  : unit -> int = "caml_polly_EFD_CLOEXEC"
+  external efd_nonblock  : unit -> int = "caml_polly_EFD_NONBLOCK"
+  external efd_semaphore  : unit -> int = "caml_polly_EFD_SEMAPHORE"
+
+  let cloexec : flags   = efd_cloexec ()
+  let nonblock : flags  = efd_nonblock ()
+  let semaphore : flags = efd_semaphore ()
+
+  let empty = 0
+
+  let all =
+    [
+      (cloexec, "cloexec")
+    ; (nonblock, "nonblock")
+    ; (semaphore, "semaphore")
+    ]
+
+  let ( lor ) = ( lor )
+
+  let ( land ) = ( land )
+
+  let lnot = lnot
+
+  let to_string t =
+    let add result (event, str) =
+      if t land event <> empty then str :: result else result
+    in
+    List.fold_left add [] all |> String.concat " "
+
+  let test x y = x land y <> empty
+
+  let read : Unix.file_descr -> int64 = fun eventfd ->
+    let buf = Bytes.create 8 in
+    assert (Unix.read eventfd buf 0 8 = 8);
+    Bytes.get_int64_ne buf 0
+
+  let add : Unix.file_descr -> int64 -> unit = fun eventfd n ->
+    let buf = Bytes.create 8 in
+    Bytes.set_int64_ne buf 0 n;
+    assert (Unix.single_write eventfd buf 0 8 = 8)
+
+end

lib/polly.mli
@@ -99,12 +99,12 @@ val wait : t -> int -> int -> (t -> Unix.file_descr -> Events.t -> unit) -> int
     over by a call to [wait]. Note that still more than [max] fds could
     be ready to be processed - they would be handled by the next call
     to [wait].
- 
+
     It is important to address the events that trigger an fd to be
     handled as otherwise the same fd will be handled again at the next
     call to [wait], leading to a tight loop. This is worth checking
     using [strace(1)].
- 
+
     See the [epoll_wait(2)] manual page for the details of the system
     call.
 
@@ -129,3 +129,49 @@ val wait_fold :
     @param f callback
     @returns number of fds ready, 0 = timeout
 *)
+
+module EventFD : sig
+
+  type t = private Unix.file_descr
+  type flags
+
+  (** [eventfd initval flags] create an evenfd with the given initial value
+      and flags. See man evenfd.*)
+
+  val eventfd  : int -> flags -> t
+
+  (** close on exec *)
+  val cloexec : flags
+
+  (** non blocking *)
+  val nonblock : flags
+
+  (** semaphore semantics, see man evenfd *)
+  val semaphore : flags
+
+  (** no flags *)
+  val empty : flags
+
+  val test : flags -> flags -> bool
+  (** [test x y] returns true, if and only if the intersection of the
+   * two sets is not empty. The common case is [test set x] where [set]
+   * is unknown and [x] is a singleton to check that [x] is contained in
+   * [set].
+   *)
+
+  val ( lor )  : flags -> flags -> flags
+  val ( land ) : flags -> flags -> flags
+  val lnot     : flags -> flags
+
+  val to_string : flags -> string
+  (** [to_string flags] return a string representation of [flags] for
+      debugging *)
+
+ (** Read the vaue of the eventfd. Block if it is zero (or trigger EAGAIN) and
+     if non zero, reset to 0 is it is not a semaphore otherwise it decrements
+     by 1.  *)
+  val read : t -> int64
+
+  (** Add the given value in the eventfd *)
+  val add : t -> int64 -> unit
+end

commit a846e52a8c3cf4979cc7325d6a24c299fdcb586a

fix(eventfd): raise exception on error
`eventfd` may fail (e.g. if you are out of file descriptors).
This needs to be handled the same way that an `epoll_create` failure is.

Fixes: 4150f12 ("Add eventfd (#4)")

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

lib/polly_stubs.c
@@ -141,6 +141,8 @@ CAMLprim value caml_polly_eventfd(value initval, value flags)
 {
 	CAMLparam0();
 	int sock = eventfd(Int_val(initval), Int_val(flags));
+	if (-1 == sock)
+		uerror(__FUNCTION__, Nothing);
 	CAMLreturn(Val_int(sock));
 }
 