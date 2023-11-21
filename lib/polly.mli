6b011243b2e30051bc84ee262cd3fa1fcb34fad4
Edwin Török <edwin.torok@cloud.com>
2023-11-21 10:55:48 +0000

    doc(Polly): remove extra * again
    A new function has sneaked in since I fixed this last time.
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- lib/polly.mli
+++ lib/polly.mli
@@ -157,9 +157,9 @@ module EventFD : sig
 
   val test : flags -> flags -> bool
   (** [test x y] returns true, if and only if the intersection of the
-   * two sets is not empty. The common case is [test set x] where [set]
-   * is unknown and [x] is a singleton to check that [x] is contained in
-   * [set].
+     two sets is not empty. The common case is [test set x] where [set]
+     is unknown and [x] is a singleton to check that [x] is contained in
+     [set].
    *)
 
   val ( lor ) : flags -> flags -> flags
